02826bbf8c9b109ad89fb3a4871092b82b431e24
Addis <addis.ugr-4143-15@aau.edu.et>
2025-07-04 16:56:14 +0300

    completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
-import Cocktails from "../components/Cocktails";
 import Hero from "../components/Hero";
+import Cocktails from "../components/Cocktails";
+import About from "../components/About";
+import Art from "../components/Art";
+import Menu from "../components/Menu";
+import Contact from "../components/Contact";
 
 const HomePage = () => {
   return (
     <div className="">
       <Hero />
       <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </div>
   );
 };
